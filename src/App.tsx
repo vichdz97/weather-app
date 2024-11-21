e486676a6511aa7f429daaa2acca7728401c3abd
Victor Hernandez, Jr <vh4697@gmail.com>
2024-11-20 18:27:53 -0600

    created weather info foundation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigation, Sun } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="flex flex-col items-center">
+			<div className="w-[200px] p-5 m-5 flex flex-col items-center text-white bg-blue-400 rounded-2xl">
+				<div className="w-3/4 flex items-center justify-evenly">
+					<Navigation color="white" fill="white" size={14} />
+					<p className="text-xs">MY LOCATION</p>
+				</div>
+				<h2 className="text-2xl">City Name</h2>
+				<h1 className="text-5xl">90&deg;</h1>
+				<div id="weather-condition" className="w-1/2 flex items-center justify-evenly text-sm ">
+					<p>Sunny</p>
+					<Sun />
+				</div>
+				<div className="w-3/4 flex justify-evenly text-sm">
+					<p>H: 100&deg;</p>
+					<p>L: 80&deg;</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App
+export default App;
