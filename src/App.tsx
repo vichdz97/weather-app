e8828e8bde8cd29f4d01f71bae909973c5438333
Victor Hernandez, Jr <vh4697@gmail.com>
2024-11-20 19:18:12 -0600

    created search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import { Navigation, Sun } from "lucide-react";
+import { Navigation, Search, Sun } from "lucide-react";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
 
 function App() {
+
 	return (
 		<div className="flex flex-col items-center">
+			<div id="search-container" className="sm:relative md:absolute top-0 right-0 mt-5 md:m-5 flex">
+				<Search size={18} strokeWidth={1} color="gray" className="absolute translate-x-1/2 translate-y-1/2" />
+				<Input 
+					type="text" 
+					placeholder="Search" 
+					className="pl-8 rounded-lg"
+				/>
+				<Button className="bg-blue-400 rounded-lg">
+					<Search />
+				</Button>
+			</div>
 			<div className="w-[200px] p-5 m-5 flex flex-col items-center text-white bg-blue-400 rounded-2xl">
 				<div className="w-3/4 flex items-center justify-evenly">
 					<Navigation color="white" fill="white" size={14} />
