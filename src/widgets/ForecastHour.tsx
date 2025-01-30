a07697b5f1d4fcfb56974caff3bfb1197776f68c
Victor Hernandez, Jr <vh4697@gmail.com>
2025-01-30 15:07:17 -0600

    only display one sunrise and one sunset

--- src/widgets/ForecastHour.tsx
+++ src/widgets/ForecastHour.tsx
@@ -23,7 +23,7 @@ function ForecastHour({ className, currentData, forecastData, getTime, getWeathe
 
     useEffect(() => {
         if (currentData && forecastData) {
-            const tempArr = [currentData, ...forecastData.list.slice(0, 10)];
+            const tempArr = [currentData, ...forecastData.list.slice(0, 9)];
             const sunrise = get24HourTime(currentData.sys.sunrise);
             const sunset = get24HourTime(currentData.sys.sunset);
 
@@ -35,24 +35,26 @@ function ForecastHour({ className, currentData, forecastData, getTime, getWeathe
                     temp: `${Math.round(tempArr[i].main.temp).toString()}°`
                 });
 
-                const timeBefore = get24HourTime(tempArr[i].dt);
-                const timeAfter = get24HourTime(tempArr[i + 1].dt);
-                if (sunrise > timeBefore && sunrise < timeAfter) {
-                    forecastArr.push({
-                        time: getTime(currentData.sys.sunrise).replace(" ", ""),
-                        icon: <Sunrise fill="gold" strokeWidth={1.5} />,
-                        temp: "Sunrise"
-                    });
-                }
-                if (sunset > timeBefore && sunset < timeAfter) {
-                    forecastArr.push({
-                        time: getTime(currentData.sys.sunset).replace(" ", ""),
-                        icon: <Sunset fill="orange" strokeWidth={1.5} />,
-                        temp: "Sunset"
-                    });
+                // prevent duplicate sunrise and sunset times
+                if (i != tempArr.length - 2) {
+                    const timeBefore = get24HourTime(tempArr[i].dt);
+                    const timeAfter = get24HourTime(tempArr[i + 1].dt);
+                    if (sunrise > timeBefore && sunrise < timeAfter) {
+                        forecastArr.push({
+                            time: getTime(currentData.sys.sunrise).replace(" ", ""),
+                            icon: <Sunrise fill="gold" strokeWidth={1.5} />,
+                            temp: "Sunrise"
+                        });
+                    }
+                    if (sunset > timeBefore && sunset < timeAfter) {
+                        forecastArr.push({
+                            time: getTime(currentData.sys.sunset).replace(" ", ""),
+                            icon: <Sunset fill="orange" strokeWidth={1.5} />,
+                            temp: "Sunset"
+                        });
+                    }
                 }
             }
-
             setWeatherData(forecastArr);
         }
 
